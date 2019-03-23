42d855eb18f1c7ea89aed23daf83b1b8b7d15bea
Allen F Partlow <allenfp@gmail.com>
2019-03-23 18:48:44 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @status == "pending"
+    if @status == "pending" and valid?
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
